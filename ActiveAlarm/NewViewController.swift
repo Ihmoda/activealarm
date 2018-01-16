a7fcbe9a0c142848292f2acc17d50c43939fde4b
Omar Ihmoda <omarihmoda@Omars-MacBook-Pro.local>
2018-01-15 16:15:59 -0800

    omar's changes

---
+++ ActiveAlarm/NewViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NewViewController.swift
+//  ActiveAlarm
+//
+//  Created by Omar Ihmoda on 1/15/18.
+//  Copyright © 2018 Omar Ihmoda. All rights reserved.
+//
+
+import UIKit
+
+class NewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
