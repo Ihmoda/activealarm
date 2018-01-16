6f90ea26b1fbef7c43773a3ef7570657818fccb9
Omar Ihmoda <omarihmoda@DN0a1f834e.SUNet>
2018-01-15 21:03:18 -0800

    changes

--- ActiveAlarm/AlarmViewController.swift
+++ ActiveAlarm/AlarmViewController.swift
@@ -11,23 +11,46 @@ import UserNotifications
 
 
 
-class AlarmViewController: UIViewController {
+class AlarmViewController: UIViewController, UNUserNotificationCenterDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+ 
     let content = UNMutableNotificationContent()
     var dateComponents = DateComponents()
+    var pickerData = ["Shake", "Walk"]
+    var selectedActivity: String = ""
     
     private var notification: NSObjectProtocol?
     
-    
     @IBOutlet weak var datePicker: UIDatePicker!
-
+    
+    @IBOutlet weak var activityPicker: UIPickerView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.registerLocal()
+        UNUserNotificationCenter.current().delegate = self
         
-        // Do any additional setup after loading the view.
+        self.activityPicker.delegate = self
+        self.activityPicker.dataSource = self
+        
     }
-        
+    
+    // The number of columns of data
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // The number of rows of data
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return self.pickerData.count
+    }
+    
+    // The data to return for the row and component (column) that's being passed in
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        self.selectedActivity = self.pickerData[row]
+        return self.pickerData[row]
+    }
 
     func setAlarm(){
         print("setAlarm")
@@ -37,22 +60,30 @@ class AlarmViewController: UIViewController {
         content.body = NSString.localizedUserNotificationString(forKey:
             "Hello_message_body", arguments: nil)
         
-        // Deliver the notification in five seconds.
+        // Deliver the notification at specified time
         content.sound = UNNotificationSound(named: "analog_watch.wav")
         let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
         
         // Schedule the notification.
-        let request = UNNotificationRequest(identifier: "FiveSecond", content: content, trigger: trigger)
+        let request = UNNotificationRequest(identifier: self.selectedActivity, content: content, trigger: trigger)
         let center = UNUserNotificationCenter.current()
         center.add(request, withCompletionHandler: nil)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        
         // Dispose of any resources that can be recreated.
     }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        print("Test: \(response.notification.request.identifier)")
         
         
+        completionHandler()
+    }
+
+
     func registerLocal() {
         print("register")
         let center = UNUserNotificationCenter.current()
@@ -68,30 +99,16 @@ class AlarmViewController: UIViewController {
     
     
     @IBAction func setAlarmPressed(_ sender: UIButton) {
-        print("Hit")
+       print("Hit")
         
-        let calendar = Calendar.current
-        let date = datePicker.date
-        let hour = calendar.component(.hour, from: date)
-        let minute = calendar.component(.minute, from: date)
-        //print(amPMSwitch.isOn)
+       let calendar = Calendar.current
+       let date = datePicker.date
+       let hour = calendar.component(.hour, from: date)
+       let minute = calendar.component(.minute, from: date)
 
-//        if let hour = datePicker.date {
-//            if !amPMSwitch.isOn {
-//                dateComponents.hour = Int(hour)
-//            } else {
-//                dateComponents.hour = Int(hour)! + 12
-//            }
-//        }
-//
-//        if let minute = minuteTextField.text {
-//            dateComponents.minute = Int(minute)
-//        }
-        dateComponents.hour = hour
-        dateComponents.minute = minute
+       dateComponents.hour = hour
+       dateComponents.minute = minute
         
-       print(dateComponents.hour)
-       print(dateComponents.minute)
        self.setAlarm()
     }
     
